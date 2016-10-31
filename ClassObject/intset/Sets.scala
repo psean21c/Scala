b4463d3c1f6a2e9a0500da0086fc7222577cd4b4
Simon Park <psean21c@gmail.com>
2016-10-31 00:20:22 -0400

    Create Sets.scala

---
+++ ClassObject/intset/Sets.scala
@@ -0,0 +1,17 @@
+class EmptySet extends IntSet{
+  def contains(x:Int):Boolean = false
+  def incl(x:Int):IntSet = new NonEmptySet(x,new EmptySet,new EmptySet)
+}
+
+class NonEmptySet(elem:Int, left:IntSet, right:IntSet) extends IntSet{
+  def contains(x:Int):Boolean = {
+    if(x < elem) left contains x
+		else if(x > elem) right contains x
+		else true
+  }
+  def incl(x:Int):IntSet ={
+      if(x< elem) new NonEmptySet(elem, left incl x, right)
+      else if(x > elem) new NonEmptySet(elem, left, right incl x)
+      else this
+    }
+}
